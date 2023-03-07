ce25df90446db5f0972c8283275431ce4a0a1b6e
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-03-07 21:56:46 +0100

    esercizio finito

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
-const containerDom = document.getElementById('container');
-let numeriInseriti=[];
+let containerDom = document.getElementById('container');
+
+
+
+//numeriscelti
+var numeriInseriti=[];
+console.log(numeriInseriti);
+
+//numeri random
 let numerivisibilicauali=[]
+console.log(numerivisibilicauali);
 
 
 
@@ -10,11 +18,21 @@ let numerivisibilicauali=[]
 
 
  numerivisibili= generatenumber(1,100);
- 
-containerDom.append(`memorizza questi numeri  ${ numerivisibili}`);
+ numerivisibilicauali.push(numerivisibili);
+ containerDom.append(`memorizza questi numeri  ${ numerivisibili}`);
 
 //imposto tempo
-setTimeout(chiedinumero, 3000);
+ setTimeout(rimuoviNumeri, 3000);
+
+  
+  
+ setTimeout(() => {
+    numerisc=chiedinumero();
+    numeriInseriti.push(numerisc);
+    confronto( numeriInseriti,numerivisibilicauali);
+ },3500);
+ 
+
 
 
 
@@ -32,11 +50,37 @@ function generatenumber(min, max){
  }
  return numericasuali;
 }
+//funzione per rimuovere i numeri
+function rimuoviNumeri() {
+    containerDom.classList.add("d-none");
+   }
 //fuzione per chiedere numero con prompt
-function chiedinumero(numerimemorizzati){
-    for(let i=0; i<5; i++){
-         numerimemorizzati=parseInt(prompt("inserisci il numero "))
-         
+function chiedinumero(){
+    var numeri=[];
+    for(var i=0; i<5; i++){
+        var numero = parseInt(prompt("Inserisci un numero"));
+        numeri.push(numero);
        }
-    return numerimemorizzati;
+       return numeri;
+}
+//funzione confronto 
+function confronto(array1, array2){
+    var punti = 0;
+
+    for (var i = 0; i < array1.length; i++) { //scorre i due array
+      if (array1[i] === array2[i]) { //se i numeri corrispondono, assegna un punto
+        punti++;
+      }
+      else { //altrimenti hai perso, restituisci il messaggio
+        alert( "Hai perso, i numeri non corrispondono");
+      }
+    }
+  
+    if (punti === 5) { //se hai tutti i punti hai vinto
+      alert("Hai vinto!");
+    }
+    else { //altrimenti hai perso, restituisci il messaggio
+      alert("Hai perso, hai ottenuto " + punti + " punti su 5");
+    }
+    
 }
